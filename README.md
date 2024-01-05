e2bc562d96de3629827f9bec4e67a7734a49e81c
ragilputra25 <gel.ragel25@gmail.com>
2024-01-05 21:01:57 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Poliklinik BK
+
+BKWD05 - Bimbingan Karier Web Development
+
+```bash
+Hendra Bagusing Ragil
+A11.2020.12564
+```
---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'poli2';
+$databaseUsername = 'root';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+
+);
+
+?>
